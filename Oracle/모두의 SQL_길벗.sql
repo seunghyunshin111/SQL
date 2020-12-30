feddf927b2b2b6fd16a50576e6d15625f4f316ce
Seunghyun Shin <57430754+seunghyunshin111@users.noreply.github.com>
2020-12-31 01:26:42 +0900

    201228

--- Oracle/모두의 SQL_길벗.sql
+++ Oracle/모두의 SQL_길벗.sql
@@ -8,3 +8,11 @@ select employee_id,
 	email || '@' || 'company.com'
 from employees;
 
+
+-- 산술 연산자
+
+select employee_id, salary, salary+500, salary-100, (salary*1.1)/2
+from employees;
+
+select employee_id, salary, salary+500 as 추가급여, salary-100 as 인하급여, (salary*1.1)/2 as 조정급여
+from employees;
