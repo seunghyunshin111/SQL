d68857703ecdd3fbcdac138bdaef88ec73a59b4f
Seunghyun Shin <57430754+seunghyunshin111@users.noreply.github.com>
2020-12-31 01:36:08 +0900

    201230

--- Oracle/모두의 SQL_길벗(join).sql
+++ Oracle/모두의 SQL_길벗(join).sql
@@ -1,10 +1,13 @@
--- 연결 연산자 |
+-- 동등 조인 : 똑같은 데이터끼리 연결하기
+-- equi-join or inner join
 
-select employee_id, first_name||last_name
-from employees;
+select *
+from employee A, departments B  -- 별칭 지정
+where A.department_id = B.department_id;
 
-select employee_id,
-	first_name || ' ' || last_name,
-	email || '@' || 'company.com'
-from employees;
+
+-- 외부 조인 : 모든 데이터를 연결하기
+select COUNT(*) 조인된 건수
+from employees A, departments B
+where A.department_id = B.department_id;
 
