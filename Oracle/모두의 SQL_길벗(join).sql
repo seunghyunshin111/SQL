ebcddce2969b218d4810f79c407e931f12dce1ce
Seunghyun Shin <57430754+seunghyunshin111@users.noreply.github.com>
2021-01-03 21:55:35 +0900

    210103

--- Oracle/모두의 SQL_길벗(join).sql
+++ Oracle/모두의 SQL_길벗(join).sql
@@ -18,3 +18,11 @@ select A.employee_id, A.first_name, A.last_name, B.department_id, B.department_n
 from employees A, departments B
 where A.department_id = B.department_id(+)
 order by A.employee_id; 
+
+
+-- 자체 조인: 자기 자신의 데이터와 연결하기
+select A.employee_id, A.first_name, A.last_name, A.manager_id,
+      B.first_name||' '||B.last_name manager_name
+from employees A, employees B
+where A.manager_id = B.employee_id
+order by A.employee_id;
